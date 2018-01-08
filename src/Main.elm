fadf1731f93ca531827f0210990d5298c731787d
James Gary <james.gary@sendgrid.com>
2018-01-08 15:16:15 -0800

    Many boids

--- src/Main.elm
+++ src/Main.elm
@@ -4,18 +4,21 @@ import AnimationFrame
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class, style)
 import Math.Vector2 as V2 exposing (Vec2, getX, getY, vec2)
+import Random
 import Time exposing (Time)
 
 
 type alias Boid =
-    { id : Int
-    , pos : Vec2
+    { pos : Vec2
     , vel : Vec2
     }
 
 
 type alias Model =
     { boids : List Boid
+    , seed : Random.Seed
+    , width : Float
+    , height : Float
     }
 
 
@@ -23,8 +26,19 @@ type Msg
     = Tick Time
 
 
+type alias Flags =
+    { timestamp : Int
+    , width : Float
+    , height : Float
+    }
+
+
+defaultSpeed =
+    1
+
+
 main =
-    Html.program
+    Html.programWithFlags
         { init = init
         , view = view
         , update = update
@@ -32,17 +46,37 @@ main =
         }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( { boids =
-            [ { id = 0
-              , pos = vec2 50 50
-              , vel = vec2 0.1 0
-              }
-            ]
+init : Flags -> ( Model, Cmd Msg )
+init { timestamp, width, height } =
+    let
+        ( boids, seed ) =
+            boidGenerator width height
+                |> Random.list 10
+                |> (\gen -> Random.step gen (Random.initialSeed timestamp))
+    in
+    ( { boids = boids
+      , width = width
+      , height = height
+      , seed = seed
       }
     , Cmd.none
     )
+
+
+boidGenerator : Float -> Float -> Random.Generator Boid
+boidGenerator width height =
+    Random.map3
+        (\x y a ->
+            { pos = vec2 x y
+            , vel =
+                ( defaultSpeed, turns a )
+                    |> fromPolar
+                    |> V2.fromTuple
+            }
+        )
+        (Random.float 0 width)
+        (Random.float 0 height)
+        (Random.float 0 1)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -62,11 +96,18 @@ view { boids } =
     div [ class "boids" ]
         (boids
             |> List.map drawBoid
+            |> List.concat
         )
 
 
-drawBoid : Boid -> Html Msg
-drawBoid { pos } =
+drawBoid : Boid -> List (Html Msg)
+drawBoid ({ pos } as boid) =
+    -- TODO: wrap
+    [ drawBoidHelper boid ]
+
+
+drawBoidHelper : Boid -> Html Msg
+drawBoidHelper { pos } =
     div
         [ class "boid"
         , style [ ( "transform", "translate(" ++ px (getX pos) ++ "," ++ px (getY pos) ++ ")" ) ]
